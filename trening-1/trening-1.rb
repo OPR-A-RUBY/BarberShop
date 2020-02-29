209dbf84b550cb404d3ae9a647a13ecb1db793fe
OPR_A <opr.a.ruby@gmail.com>
2020-03-01 00:42:50 +0400

    output "Name only"

--- trening-1/trening-1.rb
+++ trening-1/trening-1.rb
@@ -3,6 +3,6 @@ require 'sqlite3'
 db = SQLite3::Database.new 'BarberShop.db'
 
 db.execute 'SELECT * FROM db_t_visit' do |row|
-    puts row
+    puts row[1]
     puts "===================================="
 end
