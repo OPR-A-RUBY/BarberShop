dc2199fa4e97955776b5f81ff9d1baf909b4a788
OPR_A <opr.a.ruby@gmail.com>
2020-03-01 00:59:06 +0400

    output "Name and Date_Time (Hech uses)"

--- trening-1/trening-1.rb
+++ trening-1/trening-1.rb
@@ -1,8 +1,9 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'BarberShop.db'
+db.results_as_hash = true
 
 db.execute 'SELECT * FROM db_t_visit' do |row|
-    puts "#{row[1]}  #{row[3]}"
+    puts "#{row['user_name']} -\t #{row['data_time']}"
     puts "===================================="
 end
