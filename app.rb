443741cb29fe2d19d44f912c935b3fc9ce3d799c
OPR_A <opr.a.ruby@gmail.com>
2020-02-29 11:41:15 +0400

    Create db

--- app.rb
+++ app.rb
@@ -7,16 +7,28 @@ require 'sqlite3'
 configure do
     @db = SQLite3::Database.new 'BarberShop.db'
     @db.execute 'CREATE TABLE IF NOT EXISTS 
-        "Users"
+        "db_t_visit" 
         (
-            "id" INTEGER PRYMARY KEY AUTOINCREMENT,
-            "username" TEXT,
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "user_name" TEXT,
             "phone" TEXT,
-            "datestamp" TEXT,
-            "barber" TEXT,
-            "color" TEXT
+            "data_time" TEXT,
+            "barber" TEXT
         )'
-end
+
+    @db.execute 'CREATE TABLE IF NOT EXISTS 
+        "db_t_contacts" 
+        (
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "user_name" TEXT NOT NULL,
+            "user_mail" TEXT,
+            "message_user" TEXT
+        )'
+end    
+
+# rescue Exception => e
+#    
+# end
 
 get '/' do
     erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
